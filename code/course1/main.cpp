41dca633d7955613fef5da9971e5706adc1b96c1
bluove <bluove@163.com>
2018-08-21 00:47:58 +0800

    week4-over

---
+++ code/course1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>;
+//#include <hello.h>;
+using namespace std;
+
+void func1();
+void func2();
+
+int main()
+{
+	func2();
+	system("pause");
+	return 0;
+}
+
+// log, 在程序语句间加上 cout << tmp << endl; 也就是print
+// IDE, 
---
+++ code/course1/week4.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+using namespace std;
+
+void func1()
+{
+	int n, a[1000];
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	
+	cout << "input: ";
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i] << ", ";
+	}
+	cout << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 1; j < n - i; j++)
+		{
+			int tmp = 0;
+			if (a[j - 1] < a[j])
+			{
+				;
+			}
+			else
+			{
+				tmp = a[j];
+				a[j] = a[j - 1];
+				a[j - 1] = tmp;
+			}
+		}
+	}
+
+	cout << "output: ";
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i] << ", ";
+	}
+	cout << endl;
+}
+
+void func2()
+{
+	int a[10];
+//	cin >> n;
+	int n = 10;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+
+	cout << "input: ";
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i] << ", ";
+	}
+	cout << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 1; j < n - i; j++)
+		{
+			int tmp = 0;
+			bool isOrder = a[j - 1] < a[j];
+			bool isleftEven = a[j - 1] % 2 == 0;
+			bool isrightEven = a[j] % 2 == 0;
+			if ((!isleftEven && isrightEven)||(isOrder && isleftEven==isrightEven))
+			{
+				;
+			}
+			else
+			{
+				tmp = a[j];
+				a[j] = a[j - 1];
+				a[j - 1] = tmp;
+			}
+		}
+	}
+
+	cout << "output: ";
+	for (int i = 0; i < n; i++)
+	{
+		cout << a[i] << ", ";
+	}
+	cout << endl;
+}
